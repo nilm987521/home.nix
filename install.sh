0817a6bd406c7898877cbb30eb4cb5ad20f632b3
daniellan0516 <105650553+daniellan0516@users.noreply.github.com>
2022-05-22 08:41:01 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,6 +15,7 @@ home-manager switch
 # install oh-my-fish
 curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install > install
 fish install --path=~/.local/share/omf --config=~/.config/omf
+rm install
 omf install plain
 # install nnn plugins
 curl -Ls https://raw.githubusercontent.com/jarun/nnn/master/plugins/getplugs | sh
